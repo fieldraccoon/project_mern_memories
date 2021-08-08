cef398a90a932c625bcf8c2d7b18850a5d798428
fieldraccoon <fieldraccoon@gmail.com>
2021-08-08 23:20:49 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -3,18 +3,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://field:field@cluster0.vcnkk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get("/", (req,res) => {
+  res.send("Hello to Memories API")
+})
+const CONNECTION_URL = process.env.CONNECTION_URL 
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
